4945fa4b87ef75cbd73418e402b11de83199ee3b
ibrahim <barumalseyid77@gmail.com>
2026-07-12 11:14:28 +0300

    save level 1

---
+++ main.js
@@ -0,0 +1,37 @@
+let title = document.getElementById('title');
+let price = document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count');
+let category = document.getElementById('category');
+let submit = document.getElementById('submit');
+
+function getTotal(){
+   if(price.value != ''){
+      let result = (+price.value + +taxes.value + +ads.value) - discount.value;
+      total.innerHTML = result;
+      total.style.background = '#040';
+   }
+   else{
+      total.style.background = '#ff0000d7';
+   }
+}
+  // create product
+  let dataPro =[];
+submit.onclick = function(){
+   let newPro = {
+      title:title.value,
+      price:price.value,
+      taxes:taxes.value,
+      ads:ads.value,
+      discount:discount.value,
+      total:total.innerHTML,
+      count:count.value,
+      category:category.value,
+   }
+   console.log(newPro)
+}
+
+
